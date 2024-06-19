ffe1f3bbed59bf18b85b846803c392a75e5ad1ce
Mike Nguyen <mikenguyen7268@gmail.com>
2024-06-18 21:56:37 -0500

    feat: added basic UI

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,5 @@
+export default function AdminPage() {
+  return <div className="mx-auto">
+    <h1 className="text-3xl">Admin Page</h1>
+  </div>
+}
---
+++ src/pages/advertiser/index.tsx
@@ -0,0 +1,5 @@
+export default function AdvertiserPage() {
+  return <div className="mx-auto">
+    <h1 className="text-3xl">Advertiser Page</h1>
+  </div>
+}
