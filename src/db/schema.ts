4817160cb8ddc1f32c014e38485f9dde363593cf
DedsecKnight <mikenguyen7268@gmail.com>
2024-07-25 23:08:39 -0500

    feat: updated db schema to support payment

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, real } from "drizzle-orm/pg-core";
 
 export const credentialsTable = pgTable("credentials", {
   id: serial("id").primaryKey(),
@@ -27,12 +27,22 @@ export const webTable = pgTable("web", {
   numAccessed: integer("num_accessed").notNull().default(0),
 });
 
+export const advertisementTierTable = pgTable("advertisement_tier", {
+  id: serial("id").primaryKey(),
+  tierName: text("tier_name").notNull(),
+  tierPrice: real("tier_price").notNull(),
+  tierRank: integer("tier_rank").notNull(),
+});
+
 export const advertisementTable = pgTable("advertisements", {
   id: serial("id").primaryKey(),
   advertiserId: integer("advertiser_id")
     .notNull()
     .references(() => advertiserTable.id),
   content: text("content").notNull(),
+  advertisementTierId: integer("tier_id")
+    .notNull()
+    .references(() => advertisementTierTable.id),
 });
 
 export const tagTable = pgTable("tags", {
@@ -41,6 +51,24 @@ export const tagTable = pgTable("tags", {
     .notNull()
     .references(() => webTable.id),
   tagName: text("tag_name").notNull(),
+});
+
+export const billingAccountTable = pgTable("billing_accounts", {
+  id: serial("id").primaryKey(),
+  advertiserId: integer("advertiser_id")
+    .notNull()
+    .references(() => advertiserTable.id),
+  creditCardNumber: text("credit_card_number").notNull(),
+});
+
+export const transactionTable = pgTable("transaction", {
+  id: serial("id").primaryKey(),
+  billingAccountId: integer("billing_account_id")
+    .notNull()
+    .references(() => billingAccountTable.id),
+  advertisementId: integer("advertisement_id")
+    .notNull()
+    .references(() => advertisementTable.id),
 });
 
 export type InsertAdvertiser = typeof advertiserTable.$inferInsert;
